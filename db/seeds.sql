1a00c966699e3c02873b084fd31b5c80f46b44ad
bkny1020 <cgabb1020@gmail.com>
2020-11-07 01:55:57 -0500

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (1, "Classic American Cheeseburger", false);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (2, "Blue Cheese Burger", false);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (3, "Buffalo Burger", false);
